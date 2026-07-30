9e278b7644a928dc48869faea18c27c88e6926e3
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2026-07-30 08:16:10 +0200

    Use xp.max for xp array in splines.py

--- feectools/fem/splines.py
+++ feectools/fem/splines.py
@@ -93,7 +93,7 @@ class SplineSpace( FemSpace ):
         indices = xp.where(xp.diff(knots[degree:len(knots)-degree])>1e-15)[0]
 
         if len(indices)>0:
-            multiplicity = max(xp.diff(indices), default=1)
+            multiplicity = xp.max(xp.diff(indices), default=1)
         else:
             multiplicity = max(1,len(knots[degree+1:-degree-1]))
 
